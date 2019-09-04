90f7d188e72d028067804fbff84974ace18890d5
Pedro Malavazi <phmalavazi@hotmail.com>
2019-09-04 00:00:44 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -24,7 +24,17 @@ const alunoRoute = require('./routes/aluno-route');
 const pedidoRoute = require('./routes/pedido-route');
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+// Habilita o CORS
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, x-access-token');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    next();
+});
 
 app.use('/', indexRoute);
 app.use('/livro', livroRoute);
